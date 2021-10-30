1f2927f2b840d63bc64a7ba1da40df34fbd1ba31
Bryan Mendoza <bmendoza.dev@hotmail.com>
2021-10-29 23:38:54 -0600

    feat: update ESLint config file

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'next',
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    project: ['./tsconfig.json', './tsconfig.eslint.json'],
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint'],
+  root: true,
+  settings: {
+    react: { version: 'detect' },
+  },
+};
